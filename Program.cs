d4dc405b2bdcbb18db60bf8d14abfd29f50d4b51
ProfessionalUwU <andreas.fuhry@gmail.com>
2023-05-27 18:32:46 +0200

    Begin commandline arguments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program {
+    static void Main(string[] args) {
+        if (args.Length == 0) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please specify an option.");
+            Console.ResetColor();
+        } else {
+            switch(args[0]) {
+                case "":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
