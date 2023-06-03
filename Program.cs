281b6f01a2320dd4c39610d39b3240ffadaadef1
ProfessionalUwU <andreas.fuhry@gmail.com>
2023-06-03 08:51:24 +0200

    Add formating rules

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿public class Program {
-    static void Main(string[] args) {
-        if (args.Length == 0) {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Please specify an option.");
-            Console.ResetColor();
-        } else {
-            switch(args[0]) {
-                case "":
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+	static void Main(string[] args) {
+		if (args.Length == 0) {
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Please specify an option.");
+			Console.ResetColor();
+		} else {
+			switch (args[0]) {
+				case "":
+					break;
+				default:
+					break;
+			}
+		}
+	}
 }
