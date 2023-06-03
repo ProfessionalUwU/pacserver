5a09db7423c4fa443a1daf6ee1dac4b409ead714
ProfessionalUwU <andreas.fuhry@gmail.com>
2023-06-03 09:35:43 +0200

    Change formatting to spaces

--- pacserver.cs
+++ pacserver.cs
@@ -1,23 +1,23 @@
 public class Pacserver {
-	public static string pacmanCacheDirectory { get; set; } = string.Empty;
-	public static string determinePacmanCacheDirectory() {
-		return pacmanCacheDirectory;
-	}
+    public static string pacmanCacheDirectory { get; set; } = string.Empty;
+    public static string determinePacmanCacheDirectory() {
+        return pacmanCacheDirectory;
+    }
 
-	public static string pacmanDatabaseDirectory { get; set; } = string.Empty;
-	public static string determinePacmanDatabaseDirectory() {
-		return pacmanDatabaseDirectory;
-	}
+    public static string pacmanDatabaseDirectory { get; set; } = string.Empty;
+    public static string determinePacmanDatabaseDirectory() {
+        return pacmanDatabaseDirectory;
+    }
 
-	public static void checkForNewerPackagesAndDatabases() {
+    public static void checkForNewerPackagesAndDatabases() {
 
-	}
+    }
 
-	public static void transferPacmanCache() {
+    public static void transferPacmanCache() {
 
-	}
+    }
 
-	public static void transferPacmanDatabases() {
+    public static void transferPacmanDatabases() {
 
-	}
+    }
 }
