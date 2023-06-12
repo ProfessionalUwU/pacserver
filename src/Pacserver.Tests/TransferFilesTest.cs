013bc2fe33787126452760c52f1074dd51ff327f
ProfessionalUwU <andreas.fuhry@gmail.com>
2023-06-12 02:19:07 +0200

    Refactor pacserver

--- src/Pacserver.Tests/TransferFilesTest.cs
+++ src/Pacserver.Tests/TransferFilesTest.cs
@@ -4,10 +4,15 @@ namespace Pacserver.Tests;
 
 public class TranserFilesTest {
     [Fact]
-    public void TransferPacmanCacheTest() {
-        string result = PacserverUtils.determinePacmanCacheDirectory();
-        PacserverUtils.TransferPacmanCache();
+    public void transferPacmanCache_doesNotFail() {
+        // Arrange
+        PacserverUtils utils = new PacserverUtils();
 
+        // Act
+        utils.readPacmanConfig();
+        utils.transferPacmanCache();
+
+        // Assert
         //Assert.NotEmpty(Directory.GetFiles("/home/rene/test/"));
     }
 }
